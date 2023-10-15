94417986d31ee809b6090ae3aed356ec6f574dd5
JavidanGurbanli <cavidanqurbanli2003@gmail.com>
2023-10-15 20:11:40 +0400

    fix: i18n url

--- src/i18n.js
+++ src/i18n.js
@@ -14,6 +14,6 @@ i18n
     },
     order: ["localstorage"],
     backend: {
-      loadPath: "./build/public/locales/{{lng}}/{{ns}}.json",
+      loadPath: "../locales/{{lng}}/{{ns}}.json",
     }
   });
